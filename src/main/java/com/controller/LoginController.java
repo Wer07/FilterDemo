e1006fea8a3537ae07060dcccf630c6818b21ce6
wangyuwen <wyw123456>
2018-02-08 17:57:39 +0800

    first commit

---
+++ src/main/java/com/controller/LoginController.java
@@ -0,0 +1,7 @@
+package com.controller;
+
+/**
+ * Created by wyw on 2018/2/8
+ */
+public class LoginController {
+}
---
+++ src/main/java/com/filter/MyFilter.java
@@ -0,0 +1,21 @@
+package com.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by wyw on 2018/2/8
+ */
+public class MyFilter implements Filter {
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    public void destroy() {
+
+    }
+}
